2477eee6c94daaca1a5e747ffcbbf1c03f518857
Patrick <patrick@awayfromhome.nl>
2026-04-15 02:39:33 +0200

    Updated files, no theme (yet)

--- theme/awayfromhome-theme/lib/awayfromhome-theme.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "awayfromhome/theme/version"
-
-module Awayfromhome
-  module Theme
-  end
-end
--- theme/awayfromhome-theme/lib/awayfromhome/theme/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Awayfromhome
-  module Theme
-    VERSION = "0.1.0"
-  end
-end
